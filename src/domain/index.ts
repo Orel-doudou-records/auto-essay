e969a7ae8ebf76b67bac223ea6ebcd0b6c3035e0
Orel-doudou-records <orellam31@gmail.com>
2026-09-12 10:17:34 +0200

    fix: preserve existing domain exports

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -78,3 +78,6 @@ export * from "./integratedEvaluationReadiness";
 
 // Résultats intégrés historisés, consultables sans autorité d’écriture
 export * from "./integratedEvaluationHistory";
+
+// Demandes durables de lecture automatique, sans décision ni écriture
+export * from "./automaticDiffractiveReading";
