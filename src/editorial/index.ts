d800ea5b0b0e1c7d75d01fde70928cbb9fd77478
Orel-doudou-records <orellam31@gmail.com>
2026-09-13 01:15:28 +0200

    feat(plan-v2): export planning editorial plan compiler

--- src/editorial/index.ts
+++ src/editorial/index.ts
@@ -16,6 +16,7 @@ export * from "./planningSubjects.js";
 export * from "./planRefinement.js";
 export * from "./planningDecomposition.js";
 export * from "./planningStructuralDiff.js";
+export * from "./planningEditorialPlanCompiler.js";
 export * from "./manuscriptGranularity.js";
 export * from "./manuscriptReimport.js";
 export * from "./writingEngineCollaborativeCoreAdapter.js";
