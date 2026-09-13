ba126982a158391531b112e8eed39c7550890302
Orel-doudou-records <orellam31@gmail.com>
2026-09-13 17:35:31 +0200

    feat(corpus): export targeted invalidation helpers

--- src/bibliography/index.ts
+++ src/bibliography/index.ts
@@ -7,5 +7,6 @@ export * from "./corpusExplorer.js";
 export * from "./corpusSynthesis.js";
 export * from "./distribution.js";
 export * from "./citation.js";
+export * from "./invalidation.js";
 export * from "./impacts.js";
 export * from "./graphify.js";
