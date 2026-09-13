0bbd53c27c10098fb98dc080b0f69095fda1d6bd
Orel-doudou-records <orellam31@gmail.com>
2026-09-13 18:48:46 +0200

    AF-CORPUS2.10 stop exporting Graphify

--- src/bibliography/index.ts
+++ src/bibliography/index.ts
@@ -1,12 +1,7 @@
-/**
- * Index des exports de la bibliothèque (spec F).
- */
-
 export * from "./bibliography.js";
+export * from "./citation.js";
+export * from "./distribution.js";
+export * from "./impacts.js";
 export * from "./corpusExplorer.js";
 export * from "./corpusSynthesis.js";
-export * from "./distribution.js";
-export * from "./citation.js";
 export * from "./invalidation.js";
-export * from "./impacts.js";
-export * from "./graphify.js";
